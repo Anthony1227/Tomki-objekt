fa689dca11dee78fb4fc7bc56cd62f3bad39e22b
anthony tran <anth.tran00@gmail.com>
2020-11-05 09:50:09 +0100

    Add project files.

---
+++ Tomki objekt/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tomki_objekt
+{
+    class Program
+    {
+        class Person
+        {
+            public string förnamn, efternamn;
+        }
+        static void Main(string[] args)
+        {
+            //string förnamn, efternamn;
+            Person P = new Person()
+            {
+                förnamn = "Kalle",
+                efternamn = "Anka"
+            };
+            Person Q = new Person()
+            {
+                förnamn = "Kajsa",
+                efternamn = "Anka"
+            };
+        }
+    }
+}
